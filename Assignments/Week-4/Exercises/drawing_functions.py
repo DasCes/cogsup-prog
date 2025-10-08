af7321d5b30f8b1e33da10d065212c18a4617207
dario_ceni <dario01ceni@gmail.com>
2025-10-08 17:58:32 +0200

    class works session lab5

--- Assignments/Week-4/Exercises/drawing_functions.py
+++ Assignments/Week-4/Exercises/drawing_functions.py
@@ -19,67 +19,54 @@ def load(stims):
         stimulus.preload()
 
 
-def timed_draw(stims):
-
+def timed_draw(exp, stims):
     # Draw to canvas
     time_before_draw = exp.clock.time
+    exp.screen.clear()  # Clear the screen first!
     for stim in stims:
         stim.present(clear=False, update=False)
     exp.screen.update()
     time_after_draw = exp.clock.time
 
-    return time_after_draw-time_before_draw
+    return time_after_draw - time_before_draw
 
 
-def present_for(stims, t=1000):
-
-    dur = timed_draw(stims) #call here the method the give the actual process of drawing
+def present_for(exp, stims, t=1000):
+    dur = timed_draw(exp, stims)
 
     remaining_time = t - dur
     if remaining_time > 0:
         exp.clock.wait(remaining_time)
 
 
-
 """ Test functions """
-exp = design.Experiment()
+if __name__ == "__main__":
+    exp = design.Experiment()
 
-control.set_develop_mode()
-control.initialize(exp)
+    control.set_develop_mode()
+    control.initialize(exp)
 
-# defined the fixationCross stimulus and load it
-fixation = stimuli.FixCross()
-load([fixation])
+    # defined the fixationCross stimulus and load it
+    fixation = stimuli.FixCross()
+    load([fixation])
 
-# define the squares stimuli and load them
-n = 20
-positions = [(random.randint(-300, 300), random.randint(-300, 300)) for _ in range(n)]
-squares = [stimuli.Rectangle(size=(50, 50), position = pos) for pos in positions]
-load(squares)
+    # define the squares stimuli and load them
+    n = 20
+    positions = [(random.randint(-300, 300), random.randint(-300, 300)) for _ in range(n)]
+    squares = [stimuli.Rectangle(size=(50, 50), position=pos) for pos in positions]
+    load(squares)
 
-# print(len(squares))
+    durations = []
+    t0 = exp.clock.time
+    for square in squares:
+        if not square.is_preloaded:
+            print("Preloading function not implemented correctly.")
+        stims = [fixation, square]
+        present_for(exp, stims, 500)  # Pass exp here!
+        t1 = exp.clock.time
+        durations.append(t1 - t0)
+        t0 = t1
 
-# draw the fixations cross and squares, for semplicity here i can create something like stims = [fixation, square]
-# but since i want to differ the print ive done the two call separately
-# durations_drawing = timed_draw([fixation])
-# print(f"the drawing process for cross takes:  {durations_drawing/1000} seconds")
+    print(f"The presentation process at screen takes: {durations} seconds")
 
-# durations_drawing = timed_draw(squares)
-# print(f"the drawing process for squares takes:  {durations_drawing/1000} seconds")
-
-
-durations = []
-t0 = exp.clock.time
-for square in squares:
-    if not square.is_preloaded:
-        print("Preloading function not implemneted correctly.")
-    stims = [fixation, square]
-    present_for(stims, 500)
-    t1 = exp.clock.time
-    durations.append(t1-t0)
-    t0 = t1
-
-print(f"The presentation process at screen takes: {durations} seconds")
-
-# exp.keyboard.wait()
-control.end()
+    control.end()
